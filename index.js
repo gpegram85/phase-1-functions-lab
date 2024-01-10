b8aa857c8a75d4bbc837ed5cca04793cc329653c
Grey Pegram <dpegram85@gmail.com>
2024-01-09 21:23:55 -0500

    Completed Labs

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42)
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    if(distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) >= 400 && 
+                distanceTravelledInFeet(start, destination) <= 2001) {
+        return (Math.abs((destination - start) * 264) - 400) * .02
+    } else if (distanceTravelledInFeet(start, destination) > 2000 && 
+                distanceTravelledInFeet(start, destination) <= 2499) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
